b24b9c01c4d3af7ebb73053d0dce18b553dfaab3
Suliman Ibrahim <suibrahi@lab1r4s13.42abudhabi.ae>
2024-01-07 09:15:25 +0400

    still

--- main.c
+++ main.c
@@ -6,16 +6,16 @@
 /*   By: suibrahi <suibrahi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/01 14:42:10 by suibrahi          #+#    #+#             */
-/*   Updated: 2024/01/07 05:56:36 by suibrahi         ###   ########.fr       */
+/*   Updated: 2024/01/07 09:12:14 by suibrahi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+
 void *philo_action(void *arg)
 {
-		printf("%s Starting ...", arg);
-		sleep(4);
+		printf("Starting ...\n");
 	return (arg);
 }
 
@@ -29,23 +29,20 @@ int main ()
 	fork_num = 0;
 	philo_num = 1;
 	philo = NULL;
-	fork = NULL;
 	philo = malloc(sizeof(t_philo));
+	fork = malloc(sizeof(t_table));
 	while (philo_num <= 5)
 	{
-		philo[philo_num].id = philo_num;
+		philo[philo_num].id = &philo_num;
 		// printf("%d", philo[i].id);
-		pthread_create(&philo[philo_num].thread, NULL, philo_action, &philo);
+		pthread_create(&philo[philo_num].thread, NULL, philo_action, NULL);
 		philo_num++;
 	}
-	// while (1)
-	// {
-	// 	sleep(1);
-	// }
-	// while (fork_num < philo_num)
-	// {
-	// 	fork[fork_num].id = fork_num;
-	// 	pthread_mutex_init(&fork->fork_mutex, NULL);
-	// 	fork_num++;
-	// }
+		pthread_join(philo[philo_num].thread, NULL);
+	while (fork_num < philo_num)
+	{
+		fork[fork_num].id = &fork_num;
+		fork_num++;
+	}
+	printf("done\n");
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: suibrahi <suibrahi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/01 14:42:06 by suibrahi          #+#    #+#             */
-/*   Updated: 2024/01/07 05:56:28 by suibrahi         ###   ########.fr       */
+/*   Updated: 2024/01/07 09:00:32 by suibrahi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,19 @@
 # include <unistd.h>
 # include "libft/libft.h"
 
-typedef struct philosopher
-{
-	int			id;
-	pthread_t	thread; 
-}		t_philo;
-
 typedef struct table
 {
-	int 				id;
+	int					*id;
 	pthread_mutex_t		fork_mutex;
-	
+
 }		t_table;
+typedef struct philosopher
+{
+	int			*id;
+	t_table		fork;
+	pthread_t	thread;
+}		t_philo;
+
 
 void	*philo_action(void *arg);
 
